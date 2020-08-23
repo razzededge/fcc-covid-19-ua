eba9c2b7c72cac02f1148f31d61af360cd222ba0
Damian Pieszczyński <razzededge@gmail.com>
2020-08-23 19:09:42 +0200

    bulma

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,8 @@ import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
 
+import css from 'rollup-plugin-css-only'; 
+
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
@@ -29,6 +31,9 @@ export default {
 				dev,
 				hydratable: true,
 				emitCss: true
+			}),
+			css({
+				output: 'static/extra.css',
 			}),
 			resolve({
 				browser: true,
